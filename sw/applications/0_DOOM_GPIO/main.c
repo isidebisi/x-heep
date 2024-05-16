5c8940163c0b47c942faae817ee238b7b342ccf7
Ismael <ismael.frei@epfl.ch>
2024-05-16 15:34:14 +0200

    feat: added full working example of GPIO input
    preparation for DOOM

--- sw/applications/0_DOOM_GPIO/main.c
+++ sw/applications/0_DOOM_GPIO/main.c
@@ -39,23 +39,60 @@
 #endif
 
 
-#define GPIO_TB_IN  9 //AR 1
-#define GPIO_INTR  GPIO_INTR_9
+#define GPIO_TB_IN_UP  9 //RASP PI 15
+#define GPIO_INTR_UP GPIO_INTR_9
+
+#define GPIO_TB_IN_DOWN 10 //RASP PI 13
+#define GPIO_INTR_DOWN GPIO_INTR_10
+
+#define GPIO_TB_IN_LEFT 11 //RASP PI 11
+#define GPIO_INTR_LEFT GPIO_INTR_11
+
+#define GPIO_TB_IN_RIGHT 12 //RASP PI 8
+#define GPIO_INTR_RIGHT GPIO_INTR_12
+
+#define GPIO_TB_IN_A 13 //AR2
+#define GPIO_INTR_A GPIO_INTR_13
+
+#define GPIO_TB_IN_B 14 //AR3
+#define GPIO_INTR_B GPIO_INTR_14
+
+
+//local functions
+int initGPIO(uint32_t pinNum);
+int configGPIOIn(uint32_t pinNum);
+
 
 plic_result_t plic_res;
 
-uint8_t gpio_intr_flag = 0;
 
-void handler_1()
+void handler_up()
 {
-    PRINTF("handler 1\n");
-    gpio_intr_flag = 1;
+    PRINTF("UP Button pressed\n");
 }
 
-void handler_2()
+void handler_down()
 {
-    PRINTF("handler 2\n");
-    gpio_intr_flag = 1;
+    PRINTF("DOWN Button pressed\n");
+}
+void handler_left()
+{
+    PRINTF("LEFT Button pressed\n");
+}
+
+void handler_right()
+{
+    PRINTF("RIGHT Button pressed\n");
+}
+
+void handler_a()
+{
+    PRINTF("A Button pressed\n");
+}
+
+void handler_b()
+{
+    PRINTF("B Button pressed\n");
 }
 
 int main(int argc, char *argv[])
@@ -63,7 +100,6 @@ int main(int argc, char *argv[])
 
     pad_control_t pad_control;
     pad_control.base_addr = mmio_region_from_addr((uintptr_t)PAD_CONTROL_START_ADDRESS);
-    // rv_plic_params.base_addr = mmio_region_from_addr((uintptr_t)RV_PLIC_START_ADDRESS);
     plic_res = plic_Init();
     if (plic_res != kPlicOk) {
         PRINTF("Init PLIC failed\n\r;");
@@ -71,17 +107,13 @@ int main(int argc, char *argv[])
     }
 
 
-    plic_res = plic_irq_set_priority(GPIO_INTR, 1);
-    if (plic_res != kPlicOk) {
-        PRINTF("Failed\n\r;");
-        return -1;
-    }
+    initGPIO(GPIO_INTR_UP);
+    initGPIO(GPIO_INTR_DOWN);
+    initGPIO(GPIO_INTR_LEFT);
+    initGPIO(GPIO_INTR_RIGHT);
+    initGPIO(GPIO_INTR_A);
+    initGPIO(GPIO_INTR_B);
 
-    plic_res = plic_irq_set_enabled(GPIO_INTR, kPlicToggleEnabled);
-    if (plic_res != kPlicOk) {
-        PRINTF("Failed\n\r;");
-        return -1;
-    }
 
     // Enable interrupt on processor side
     // Enable global interrupt for machine-level interrupts
@@ -90,49 +122,62 @@ int main(int argc, char *argv[])
     const uint32_t mask = 1 << 11;
     CSR_SET_BITS(CSR_REG_MIE, mask);
 
-    //gpio_reset_all();
+    
+
+    configGPIOIn(GPIO_TB_IN_UP);
+    configGPIOIn(GPIO_TB_IN_DOWN);
+    configGPIOIn(GPIO_TB_IN_LEFT);
+    configGPIOIn(GPIO_TB_IN_RIGHT);
+    configGPIOIn(GPIO_TB_IN_A);
+    configGPIOIn(GPIO_TB_IN_B);
+
+
+
+    gpio_assign_irq_handler( GPIO_INTR_UP, &handler_up );
+    gpio_assign_irq_handler( GPIO_INTR_DOWN, &handler_down );
+    gpio_assign_irq_handler( GPIO_INTR_LEFT, &handler_left );
+    gpio_assign_irq_handler( GPIO_INTR_RIGHT, &handler_right );
+    gpio_assign_irq_handler( GPIO_INTR_A, &handler_a );
+    gpio_assign_irq_handler( GPIO_INTR_B, &handler_b );
+
+    PRINTF("Press Buttons for interrupt...\n\r");
+    while(1) {
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+int initGPIO(uint32_t pinNum)
+{
+        plic_res = plic_irq_set_priority(pinNum, 1);
+    if (plic_res != kPlicOk) {
+        PRINTF("Failed\n\r;");
+        return -1;
+    }
+
+    plic_res = plic_irq_set_enabled(pinNum, kPlicToggleEnabled);
+    if (plic_res != kPlicOk) {
+        PRINTF("Failed\n\r;");
+        return -1;
+    }
+}
+
+int configGPIOIn(uint32_t pinNum)
+{
     gpio_result_t gpio_res;
 
-
     gpio_cfg_t cfg_in = {
-        .pin = GPIO_TB_IN,
+        .pin = pinNum,
         .mode = GpioModeIn,
         .en_input_sampling = true,
         .en_intr = true,
         .intr_type = GpioIntrEdgeFalling
     };
+
     gpio_res = gpio_config(cfg_in);
     if (gpio_res != GpioOk) {
         PRINTF("Failed\n;");
         return -1;
     }
-
-    gpio_assign_irq_handler( GPIO_INTR, &handler_1 );
-    gpio_intr_flag = 0;
-
-    PRINTF("Press Button for interrupt...\n\r");
-    while(gpio_intr_flag == 0) {
-        // disable_interrupts
-        // this does not prevent waking up the core as this is controlled by the MIP register
-        CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
-        // wait_for_interrupt();GPIO
-        CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
-        PRINTF("Wait for button in PIN AR1 press for interrupt...\n\r");
-    }
-
-    gpio_assign_irq_handler( GPIO_INTR, &handler_2 );
-    gpio_intr_flag = 0;
-
-    PRINTF("Press Button for interrupt...\n\r");
-    while(gpio_intr_flag == 0) {
-        // disable_interrupts
-        // this does not prevent waking up the core as this is controlled by the MIP register
-        CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
-        //wait_for_interrupt();
-        CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
-    }
-
-    PRINTF("Success\n\r");
-
-    return EXIT_SUCCESS;
 }
